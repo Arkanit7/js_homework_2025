8d1dcb0a185d6f2023566ae1ee7ef33df1dcd3f9
BorisBedrenko <wadeforce@gmail.com>
2025-02-02 18:46:04 +0200

    new home page layout

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,10 +1,30 @@
-const dateString = "2025-02-01";
-const date = new Date(dateString);
-
-const ukrainianDate = date.toLocaleDateString('uk-UA', {
-  year: '',
-  month: 'long',
-  day: '2-digit',
-});
-
-console.log(ukrainianDate); 
+const lessons = [
+  {
+    href: './lesson_02/',
+    title: 'Домашня робота №2',
+    img: { src: 'homework-placeholder-2.jpg' },
+    number: 2,
+    datetime: new Date('2025-01-28'),
+  },
+  {
+    href: './lesson_03/',
+    title: 'Домашня робота №3',
+    img: { src: 'homework-placeholder-3.jpg' },
+    number: 3,
+    datetime: new Date('2025-01-30'),
+  },
+  {
+    href: './lesson_05/',
+    title: 'Домашня робота №6',
+    img: { src: 'homework-placeholder-5.jpg' },
+    number: 2,
+    datetime: new Date('2025-01-28'),
+  },
+  {
+    href: './lesson_06/',
+    title: 'Домашня робота №6',
+    img: { src: 'homework-placeholder-6.jpg' },
+    number: 3,
+    datetime: new Date('2025-01-30'),
+  },
+]
