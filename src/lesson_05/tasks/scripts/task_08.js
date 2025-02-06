c9eece672cd6b2ec684b5f76eb64b1e7255a16e5
BorisBedrenko <wadeforce@gmail.com>
2025-02-06 12:01:10 +0200

    observations: task 08

--- src/lesson_05/tasks/scripts/task_08.js
+++ src/lesson_05/tasks/scripts/task_08.js
@@ -1,28 +1,29 @@
 /*
 ## Алгоритм
-кількість спроб комп'ютера ATTEMPTS
-мінімальне число MIN_RANDOM_NUMBER
-максимальне число MAX_RANDOM_NUMBER
-повідомлення до користувача message
+ввести кількість спроб комп'ютера ATTEMPTS
+ввести мінімальне число MIN_RANDOM_NUMBER
+ввести максимальне число MAX_RANDOM_NUMBER
+оголосити чи користувач вгадав rightGuess 
 
 повторювати для ATTEMPTS
   - ввести випадкове число randomNumber
-  - запитати користувача чи вірне число
+  - запитати користувача чи вгадав rightGuess
   - якщо відповідь вірна
     то
-      - записати переможне повідомлення message
       - припинити цикл
-    інакше
-      - записати невтішне повідомлення message
-      
-вивести message
+
+якщо rightGuess
+  то
+    - переможне повідомлення
+  інакше
+    - невтішне повідомлення
 */
 
 if (confirm('Почати тестування?')) {
   const ATTEMPTS = 3
   const MIN_RANDOM_NUMBER = 0,
     MAX_RANDOM_NUMBER = 5
-  let message
+  let rightGuess
 
   alert(`Загадайте число від ${MIN_RANDOM_NUMBER} до ${MAX_RANDOM_NUMBER}`)
 
@@ -30,13 +31,11 @@ if (confirm('Почати тестування?')) {
     const randomNumber =
       MIN_RANDOM_NUMBER +
       Math.floor(Math.random() * (MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1))
-    const rightGuess = confirm(`Чи ваше число це – ${randomNumber}?`)
+    rightGuess = confirm(`Чи ваше число це – ${randomNumber}?`)
 
-    if (rightGuess) {
-      message = `Я вгадав ваше число ${randomNumber}!`
-      break
-    } else message = `Мені не вдалося вгадати ваше число.`
+    if (rightGuess) break
   }
 
-  document.write(message)
+  if (rightGuess) document.write(`Я вгадав ваше число!`)
+  else document.write(`Мені не вдалося вгадати ваше число.`)
 }
