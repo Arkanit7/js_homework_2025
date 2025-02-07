3dee140913b94d1d56a1a3a5bc21c420a3ee5373
BorisBedrenko <wadeforce@gmail.com>
2025-02-07 22:35:56 +0200

    lesson 5. task 1: change ol => ul

--- src/lesson_05/tasks/scripts/task_01.js
+++ src/lesson_05/tasks/scripts/task_01.js
@@ -3,13 +3,13 @@
 ввести кількість випадкових чисел numbersAmount
 ввести мінімальне випадкове число MIN_RANDOM_NUMBER
 ввести максимальне випадкове число MAX_RANDOM_NUMBER
-вивести початок списку ol
+вивести початок списку ul
 
 повторювати numbersAmount разів
   - згенерувати випадкове число randomNumber
   - вивести randomNumber як елемент списку
 
-вивести кінець списку ol
+вивести кінець списку ul
 */
 
 const MIN_RANDOM_NUMBER = 1
@@ -18,7 +18,7 @@ const numbersAmount = parseInt(
   prompt('Введіть кількість випадкових чисел', '10'),
 )
 
-document.write('<ol>')
+document.write('<ul>')
 
 for (let i = 0; i < numbersAmount; i++) {
   const randomNumber =
@@ -28,4 +28,4 @@ for (let i = 0; i < numbersAmount; i++) {
   document.write(`<li>${randomNumber}</li>`)
 }
 
-document.write('</ol>')
+document.write('</ul>')
