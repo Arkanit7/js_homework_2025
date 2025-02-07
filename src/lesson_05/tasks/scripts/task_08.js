37aa0fd1f755ad72af5988be3f07217bca4831be
BorisBedrenko <wadeforce@gmail.com>
2025-02-07 16:51:48 +0200

    lesson 5: rewrite task 8

--- src/lesson_05/tasks/scripts/task_08.js
+++ src/lesson_05/tasks/scripts/task_08.js
@@ -3,14 +3,11 @@
 ввести кількість спроб комп'ютера ATTEMPTS
 ввести мінімальне число MIN_RANDOM_NUMBER
 ввести максимальне число MAX_RANDOM_NUMBER
-оголосити чи користувач вгадав rightGuess 
+ввести чи користувач вгадав rightGuess = false
 
-повторювати для ATTEMPTS
-  - ввести випадкове число randomNumber
+повторювати для ATTEMPTS, доки не rightGuess
+  - згенерувати випадкове число randomNumber
   - запитати користувача чи вгадав rightGuess
-  - якщо відповідь вірна
-    то
-      - припинити цикл
 
 якщо rightGuess
   то
@@ -23,17 +20,16 @@ if (confirm('Почати тестування?')) {
   const ATTEMPTS = 3
   const MIN_RANDOM_NUMBER = 0,
     MAX_RANDOM_NUMBER = 5
-  let rightGuess
+  let rightGuess = false
 
   alert(`Загадайте число від ${MIN_RANDOM_NUMBER} до ${MAX_RANDOM_NUMBER}`)
 
-  for (let i = 0; i < ATTEMPTS; i++) {
+  for (let i = 0; i < ATTEMPTS && !rightGuess; i++) {
     const randomNumber =
       MIN_RANDOM_NUMBER +
       Math.floor(Math.random() * (MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1))
-    rightGuess = confirm(`Чи ваше число це – ${randomNumber}?`)
 
-    if (rightGuess) break
+    rightGuess = confirm(`Чи ваше число це – ${randomNumber}?`)
   }
 
   if (rightGuess) document.write(`Я вгадав ваше число!`)
