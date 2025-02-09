d625453fc0b6b88b895e65529cca3086327b2ded
BorisBedrenko <wadeforce@gmail.com>
2025-02-09 19:53:47 +0200

    lesson 6: rewrite task 28

--- src/lesson_06/tasks/scripts/task_28.js
+++ src/lesson_06/tasks/scripts/task_28.js
@@ -1,18 +1,37 @@
 if (confirm('Почати тестування?')) {
-  const dayStartAt = parseFloat(
-    prompt('Коли починається навальний день, у годинах?', '8.5'),
+  const dayStartHour = parseInt(
+    prompt('О котрій годині починається навчальний день? (0 - 23)', '8'),
   )
-  const lessonDuration =
-    parseFloat(prompt('Яка тривалість уроку, у хвилинах?', 45)) / 60
-  const lessonAmount = parseFloat(prompt('Кількість уроків?', '6'))
+  const dayStartMinute = parseInt(
+    prompt(`О ${dayStartHour} годині та скільки хвилин? (0-59)`, '30'),
+  )
+  const lessonDurationMinutes = parseInt(
+    prompt('Яка тривалість уроку, у хвилинах?', '45'),
+  )
+  const recessDurationMinutes = parseInt(
+    prompt('А тривалість перерви, у хвилинах?', '15'),
+  )
+  const lessonAmount = parseInt(prompt('Кількість уроків?', '6'))
 
-  let time = dayStartAt
+  let currentHour = dayStartHour
+  let currentMinute = dayStartMinute
 
-  for (let lessonCount = 1; lessonCount < lessonAmount; lessonCount++) {
-    document.write(`<p>Урок №${lessonCount} ${time.toFixed(2)} - `)
-    time += lessonDuration
-    document.write(`${time.toFixed(2)}`)
+  for (let lessonCount = 1; lessonCount <= lessonAmount; lessonCount++) {
+    document.write(
+      `<p>Урок №${lessonCount} ${String(currentHour).padStart(2, '0')}:${String(currentMinute).padStart(2, '0')} - `,
+    )
+    currentMinute += lessonDurationMinutes
+    currentHour += Math.floor(currentMinute / 60)
+    currentHour %= 24
+    currentMinute %= 60
+
+    document.write(
+      `${String(currentHour).padStart(2, '0')}:${String(currentMinute).padStart(2, '0')}`,
+    )
+
+    currentMinute += recessDurationMinutes
+    currentHour += Math.floor(currentMinute / 60)
+    currentHour %= 24
+    currentMinute %= 60
   }
-
-  document.write(`<p>Вчимося без жодних перерв! :D`)
 }
