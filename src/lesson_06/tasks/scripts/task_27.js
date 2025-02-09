4358946374c523cfaf1f0a8537ebed0d470d46c6
BorisBedrenko <wadeforce@gmail.com>
2025-02-09 19:23:46 +0200

    lesson 6: rewrite task 27

--- src/lesson_06/tasks/scripts/task_27.js
+++ src/lesson_06/tasks/scripts/task_27.js
@@ -1,41 +1,19 @@
 // ## Алгоритм Евкліда
-// НСД для різниці двох чисел буде спільним для всіх чисел
+// НСД не змінюється, якщо від більшого числа відняти менше
 
 if (confirm('Почати тестування?')) {
   let firstInteger = parseInt(prompt('Введіть перше число', '105'))
   let secondInteger = parseInt(prompt('Введіть друге число', '252'))
 
   document.write(`НСД для ${firstInteger} та ${secondInteger} = `)
-  firstInteger = Math.abs(firstInteger)
-  secondInteger = Math.abs(secondInteger)
 
-  while (firstInteger && secondInteger) {
-    const temp = Math.abs(secondInteger - firstInteger)
-
-    secondInteger = firstInteger
-    firstInteger = temp
+  while (secondInteger && firstInteger) {
+    if (firstInteger > secondInteger) {
+      firstInteger %= secondInteger
+    } else {
+      secondInteger %= firstInteger
+    }
   }
 
   document.write(firstInteger || secondInteger)
 }
-
-// ## варіант 2
-
-// if (confirm('Почати тестування?')) {
-//   let firstInteger = parseInt(prompt('Введіть перше число', '105'))
-//   let secondInteger = parseInt(prompt('Введіть друге число', '252'))
-
-//   document.write(`НСД для ${firstInteger} та ${secondInteger} = `)
-//   firstInteger = Math.abs(firstInteger)
-//   secondInteger = Math.abs(secondInteger)
-
-//   while (secondInteger !== 0 && firstInteger !== 0) {
-//     if (firstInteger > secondInteger) {
-//       firstInteger %= secondInteger
-//     } else {
-//       secondInteger %= firstInteger
-//     }
-//   }
-
-//   document.write(firstInteger || secondInteger)
-// }
