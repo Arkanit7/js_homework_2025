5c90d3d522d520b5e9afa89077ebb7541a3e247b
BorisBedrenko <wadeforce@gmail.com>
2025-02-10 14:12:54 +0200

    lesson 6: optimize task 32

--- src/lesson_06/tasks/scripts/task_32.js
+++ src/lesson_06/tasks/scripts/task_32.js
@@ -1,14 +1,11 @@
 if (confirm('Почати тестування?')) {
   let redBacteria = parseInt(prompt('Введіть кількість червоних бактерій', '1'))
   let greenBacteria = 0
-  const duration = parseInt(prompt('Введіть кількість одиниць часу', '40'))
+  const duration = parseInt(prompt('Введіть кількість одиниць часу', '30'))
 
   for (let time = 1; time <= duration; time++) {
-    const newGreenBacteria = greenBacteria + redBacteria
-    const newRedBacteria = greenBacteria
-
-    greenBacteria = newGreenBacteria
-    redBacteria = newRedBacteria
+    greenBacteria += redBacteria
+    redBacteria = greenBacteria - redBacteria
   }
 
   document.write(`Усіх бактерій: ${redBacteria + greenBacteria}`)
