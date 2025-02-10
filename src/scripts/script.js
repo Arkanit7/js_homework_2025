9e512287ef99ea0cec75e5cf65fd470a34a9e5db
BorisBedrenko <wadeforce@gmail.com>
2025-02-10 22:09:25 +0200

    Add: search bar for tasks page

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,3 +5,32 @@ const scrollIntoView = () => {
 }
 
 scrollIntoView()
+
+/**
+ * @param {InputEvent} e
+ * @param {HTMLAllCollection} tasks
+ */
+const handleSearch = (e, tasks) => {
+  const query = e.currentTarget.value.trim().toLowerCase()
+
+  console.log(query)
+
+  tasks.forEach((task) => {
+    if (task.innerText.toLowerCase().includes(query)) task.style.display = ''
+    else task.style.display = 'none'
+  })
+}
+
+const search = () => {
+  const searchInput = document.querySelector('.js-search-input')
+  const allTasks = document.querySelectorAll('.js-search-task')
+
+  if (!searchInput) return
+  if (!allTasks) return
+
+  searchInput.addEventListener('input', (e) => {
+    handleSearch(e, allTasks)
+  })
+}
+
+search()
