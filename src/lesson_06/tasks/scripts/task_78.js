5ef398128c54b2c76269d3fee28a3bfd0dea0b67
BorisBedrenko <wadeforce@gmail.com>
2025-02-19 15:33:55 +0200

    update: Swap L6:78 for review. New logic 🟫

--- src/lesson_06/tasks/scripts/task_78.js
+++ src/lesson_06/tasks/scripts/task_78.js
@@ -1,41 +1,44 @@
 if (confirm('Почати тестування?')) {
-  const SIZE = 10
+  const FIELD_SIZE = 10
+
   let ammo = parseInt(prompt('Кількість снарядів 🖍️:', '20🖍️'))
+  let shotsCount = 0
+
   let playerLife = 100
   let tankLife = 100
-  let shotsCount = 1
-  let hasWon = false
+
   let shotX,
-    tankX = Math.floor(Math.random() * (SIZE - 1))
+    tankX = Math.floor(Math.random() * (FIELD_SIZE - 1))
 
-  for (; shotsCount <= ammo && playerLife > 0; shotsCount++) {
-    let field = `🧱Танчики🧱
-Снаряди: ${ammo - shotsCount + 1}🖍️
-Ваше життя: ${playerLife}❤️
-Т-90: ${tankLife}🚜
-──────────────────────
-🧱 1   2   3   4   5   6   7   8   9  10 🧱
-🧱`
+  for (; shotsCount < ammo && playerLife > 0 && tankLife > 0; shotsCount++) {
+    // Заголовок та ігрове табло
+    let field = '🧱Танчики🧱\n'
+    field += `Снаряди: ${ammo - shotsCount}🖍️\n`
+    field += `Ваше життя: ${playerLife}❤️\n`
+    field += `Т-90: ${tankLife}🚜\n`
+    field += '──────────────────────\n'
+    field += '🧱 1   2   3   4   5   6   7   8   9  10 🧱\n'
+    field += '🧱'
 
-    for (let col = 0; col < SIZE; col++) {
+    // Побудова ігрового поля
+    for (let col = 0; col < FIELD_SIZE; col++) {
       if (col === tankX) field += '🚜'
-      else field += '〰️'
+      else field += '🟫'
     }
 
     field += '🧱🔫\n──────────────────────'
-    shotX = parseInt(prompt(`${field}\nПозиція пострілу (1-${SIZE}):`, '5')) - 1
+
+    shotX =
+      parseInt(prompt(`${field}\nПозиція пострілу (1-${FIELD_SIZE}):`, '5')) - 1
     tankX += -1 + Math.floor(Math.random() * 3)
 
     // Танк не може виїхати з поля
-    if (tankX > SIZE - 1) tankX = SIZE - 1 - Math.floor(Math.random() * 2)
+    if (tankX > FIELD_SIZE - 1)
+      tankX = FIELD_SIZE - 1 - Math.floor(Math.random() * 2)
     if (tankX < 0) tankX = Math.floor(Math.random() * 2)
 
     // Життя танка
     if (shotX === tankX) tankLife -= 30
-    if (tankLife < 0) {
-      hasWon = true
-      break
-    }
 
     // Ймовірність попадання у гармату гравця (25%)
     const tankShotX = 0 + Math.floor(Math.random() * 4)
@@ -43,9 +46,10 @@ if (confirm('Почати тестування?')) {
     if (tankShotX === 0) playerLife -= 30
   }
 
-  if (hasWon)
+  if (tankLife < 0)
     alert(
-      `Ворожий Т-90 знищено! Загальна кількість пострілів: ${shotsCount}🖍️.`,
+      `🎖️Ворожий Т-90 знищено!🎖️ 🔥🚜🔥\nЗагальна кількість пострілів: ${shotsCount}🖍️.`,
     )
-  else alert('Танк прорвався!')
+  else if (playerLife < 0) alert(`💥Вашу гармату знищено! 🔥🔫🔥`)
+  else alert('🪓 У вас закінчилися снаряди 🖍️!')
 }
