390ce7e5a78cedfc1eed207dcaed29886ec4fe41
BorisBedrenko <wadeforce@gmail.com>
2025-03-06 15:06:43 +0200

    fix: L10:45 function wrong name

--- src/lesson_10/tasks/scripts/task_45.js
+++ src/lesson_10/tasks/scripts/task_45.js
@@ -10,7 +10,7 @@ function printTest(expect, toBe) {
  * @param {any[]} array
  * @returns {number}
  */
-function countSignChange(array) {
+function countUnique(array) {
   // return new Set(array).size
 
   return array.reduce((unique, item) => {
@@ -20,14 +20,14 @@ function countSignChange(array) {
   }, []).length
 }
 
-printTest(countSignChange([1, 2, 3, 4]), 4)
-printTest(countSignChange([4, 3, 2, 1]), 4)
-printTest(countSignChange([1, 3, 2, 1]), 3)
-printTest(countSignChange([1, 1]), 1)
-printTest(countSignChange([1, 2]), 2)
-printTest(countSignChange([1, 1, 2, 2]), 2)
-printTest(countSignChange([1, 2, 2, 3]), 3)
-printTest(countSignChange([2, 2, 2, 2]), 1)
-printTest(countSignChange(['cat', 'cat']), 1)
-printTest(countSignChange(['cat', 'kitty']), 2)
-printTest(countSignChange(['cat']), 1)
+printTest(countUnique([1, 2, 3, 4]), 4)
+printTest(countUnique([4, 3, 2, 1]), 4)
+printTest(countUnique([1, 3, 2, 1]), 3)
+printTest(countUnique([1, 1]), 1)
+printTest(countUnique([1, 2]), 2)
+printTest(countUnique([1, 1, 2, 2]), 2)
+printTest(countUnique([1, 2, 2, 3]), 3)
+printTest(countUnique([2, 2, 2, 2]), 1)
+printTest(countUnique(['cat', 'cat']), 1)
+printTest(countUnique(['cat', 'kitty']), 2)
+printTest(countUnique(['cat']), 1)
