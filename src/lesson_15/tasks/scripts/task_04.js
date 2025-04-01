6523f4169876f70064557f0013fd7a798cee60c0
BorisBedrenko <wadeforce@gmail.com>
2025-04-01 22:21:40 +0300

    refactor: L15:4 use a class to generate banners

--- src/lesson_15/tasks/scripts/task_04.js
+++ src/lesson_15/tasks/scripts/task_04.js
@@ -1,12 +1,5 @@
 'use strict'
 
-/**
- * @typedef {Object} SiteBanner
- * @property {string} href - A link to the website.
- * @property {string} imgSrc - A path to the image.
- * @property {string} imgAlt - imgAlternative text for the image.
- */
-
 /** @type {(from: number, to: number) => number} */
 function generateIntegerInRange(from, to) {
   return from + Math.floor(Math.random() * (to - from + 1))
@@ -41,7 +34,7 @@ class Banner {
     return this.bannersList[randomIndex]
   }
 
-  renderRandomBanner(times = 1) {
+  renderRandomBanners(times = 1) {
     for (let i = 0; i < times; i++) {
       const { href, imgSrc, imgAlt } = this.getRandomBanner()
 
@@ -54,137 +47,150 @@ class Banner {
   }
 }
 
+class SiteBanner {
+  /**
+   * @param {string} href - A link, to which the banner will lead.
+   * @param {string} imgSrc - A source for the image file.
+   * @param {string} imgAlt - An alternative accessability text for the image.
+   */
+  constructor(href, imgSrc, imgAlt) {
+    this.href = href
+    this.imgSrc = imgSrc
+    this.imgAlt = imgAlt
+  }
+}
+
 /** @type {SiteBanner[]} */
-const jsNodeBanners = [
-  {
-    href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-    imgSrc: 'https://cdn.simpleicons.org/javascript',
-    imgAlt: 'JavaScript',
-  },
-  {
-    href: 'https://nodejs.org/',
-    imgSrc: 'https://cdn.simpleicons.org/nodedotjs',
-    imgAlt: 'Node.js',
-  },
-  {
-    href: 'https://www.npmjs.com/',
-    imgSrc: 'https://cdn.simpleicons.org/npm',
-    imgAlt: 'npm',
-  },
-  {
-    href: 'https://yarnpkg.com/',
-    imgSrc: 'https://cdn.simpleicons.org/yarn',
-    imgAlt: 'Yarn',
-  },
-  {
-    href: 'https://webpack.js.org/',
-    imgSrc: 'https://cdn.simpleicons.org/webpack',
-    imgAlt: 'Webpack',
-  },
-  {
-    href: 'https://rollupjs.org/',
-    imgSrc: 'https://cdn.simpleicons.org/rollupdotjs',
-    imgAlt: 'Rollup.js',
-  },
-  {
-    href: 'https://vercel.com/',
-    imgSrc: 'https://cdn.simpleicons.org/vercel',
-    imgAlt: 'Vercel',
-  },
-  {
-    href: 'https://react.dev/',
-    imgSrc: 'https://cdn.simpleicons.org/react',
-    imgAlt: 'React',
-  },
-  {
-    href: 'https://vuejs.org/',
-    imgSrc: 'https://cdn.simpleicons.org/vuedotjs',
-    imgAlt: 'Vue.js',
-  },
-  {
-    href: 'https://angular.io/',
-    imgSrc: 'https://cdn.simpleicons.org/angular',
-    imgAlt: 'Angular',
-  },
-  {
-    href: 'https://expressjs.com/',
-    imgSrc: 'https://cdn.simpleicons.org/express',
-    imgAlt: 'Express',
-  },
-  {
-    href: 'https://nestjs.com/',
-    imgSrc: 'https://cdn.simpleicons.org/nestjs',
-    imgAlt: 'NestJS',
-  },
-  {
-    href: 'https://socket.io/',
-    imgSrc: 'https://cdn.simpleicons.org/socketdotio',
-    imgAlt: 'Socket.IO',
-  },
-  {
-    href: 'https://jestjs.io/',
-    imgSrc: 'https://cdn.simpleicons.org/jest',
-    imgAlt: 'Jest',
-  },
-  {
-    href: 'https://mochajs.org/',
-    imgSrc: 'https://cdn.simpleicons.org/mocha',
-    imgAlt: 'Mocha',
-  },
-  {
-    href: 'https://www.cypress.io/',
-    imgSrc: 'https://cdn.simpleicons.org/cypress',
-    imgAlt: 'Cypress',
-  },
-  {
-    href: 'https://eslint.org/',
-    imgSrc: 'https://cdn.simpleicons.org/eslint',
-    imgAlt: 'ESLint',
-  },
-  {
-    href: 'https://prettier.io/',
-    imgSrc: 'https://cdn.simpleicons.org/prettier',
-    imgAlt: 'Prettier',
-  },
-  {
-    href: 'https://www.typescriptlang.org/',
-    imgSrc: 'https://cdn.simpleicons.org/typescript',
-    imgAlt: 'TypeScript',
-  },
-  {
-    href: 'https://babeljs.io/',
-    imgSrc: 'https://cdn.simpleicons.org/babel',
-    imgAlt: 'Babel',
-  },
-  {
-    href: 'https://jquery.com/',
-    imgSrc: 'https://cdn.simpleicons.org/jquery',
-    imgAlt: 'jQuery',
-  },
-  {
-    href: 'https://lodash.com/',
-    imgSrc: 'https://cdn.simpleicons.org/lodash',
-    imgAlt: 'Lodash',
-  },
-  {
-    href: 'https://handlebarsjs.com/',
-    imgSrc: 'https://cdn.simpleicons.org/handlebarsdotjs',
-    imgAlt: 'Handlebars.js',
-  },
-  {
-    href: 'https://sequelize.org/',
-    imgSrc: 'https://cdn.simpleicons.org/sequelize',
-    imgAlt: 'Sequelize',
-  },
-  {
-    href: 'https://mongoosejs.com/',
-    imgSrc: 'https://cdn.simpleicons.org/mongoose',
-    imgAlt: 'Mongoose',
-  },
+const jsBanners = [
+  new SiteBanner(
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    'https://cdn.simpleicons.org/javascript',
+    'JavaScript',
+  ),
+  new SiteBanner(
+    'https://nodejs.org/',
+    'https://cdn.simpleicons.org/nodedotjs',
+    'Node.js',
+  ),
+  new SiteBanner(
+    'https://www.npmjs.com/',
+    'https://cdn.simpleicons.org/npm',
+    'npm',
+  ),
+  new SiteBanner(
+    'https://yarnpkg.com/',
+    'https://cdn.simpleicons.org/yarn',
+    'Yarn',
+  ),
+  new SiteBanner(
+    'https://webpack.js.org/',
+    'https://cdn.simpleicons.org/webpack',
+    'Webpack',
+  ),
+  new SiteBanner(
+    'https://rollupjs.org/',
+    'https://cdn.simpleicons.org/rollupdotjs',
+    'Rollup.js',
+  ),
+  new SiteBanner(
+    'https://vercel.com/',
+    'https://cdn.simpleicons.org/vercel',
+    'Vercel',
+  ),
+  new SiteBanner(
+    'https://react.dev/',
+    'https://cdn.simpleicons.org/react',
+    'React',
+  ),
+  new SiteBanner(
+    'https://vuejs.org/',
+    'https://cdn.simpleicons.org/vuedotjs',
+    'Vue.js',
+  ),
+  new SiteBanner(
+    'https://angular.io/',
+    'https://cdn.simpleicons.org/angular',
+    'Angular',
+  ),
+  new SiteBanner(
+    'https://expressjs.com/',
+    'https://cdn.simpleicons.org/express',
+    'Express',
+  ),
+  new SiteBanner(
+    'https://nestjs.com/',
+    'https://cdn.simpleicons.org/nestjs',
+    'NestJS',
+  ),
+  new SiteBanner(
+    'https://socket.io/',
+    'https://cdn.simpleicons.org/socketdotio',
+    'Socket.IO',
+  ),
+  new SiteBanner(
+    'https://jestjs.io/',
+    'https://cdn.simpleicons.org/jest',
+    'Jest',
+  ),
+  new SiteBanner(
+    'https://mochajs.org/',
+    'https://cdn.simpleicons.org/mocha',
+    'Mocha',
+  ),
+  new SiteBanner(
+    'https://www.cypress.io/',
+    'https://cdn.simpleicons.org/cypress',
+    'Cypress',
+  ),
+  new SiteBanner(
+    'https://eslint.org/',
+    'https://cdn.simpleicons.org/eslint',
+    'ESLint',
+  ),
+  new SiteBanner(
+    'https://prettier.io/',
+    'https://cdn.simpleicons.org/prettier',
+    'Prettier',
+  ),
+  new SiteBanner(
+    'https://www.typescriptlang.org/',
+    'https://cdn.simpleicons.org/typescript',
+    'TypeScript',
+  ),
+  new SiteBanner(
+    'https://babeljs.io/',
+    'https://cdn.simpleicons.org/babel',
+    'Babel',
+  ),
+  new SiteBanner(
+    'https://jquery.com/',
+    'https://cdn.simpleicons.org/jquery',
+    'jQuery',
+  ),
+  new SiteBanner(
+    'https://lodash.com/',
+    'https://cdn.simpleicons.org/lodash',
+    'Lodash',
+  ),
+  new SiteBanner(
+    'https://handlebarsjs.com/',
+    'https://cdn.simpleicons.org/handlebarsdotjs',
+    'Handlebars.js',
+  ),
+  new SiteBanner(
+    'https://sequelize.org/',
+    'https://cdn.simpleicons.org/sequelize',
+    'Sequelize',
+  ),
+  new SiteBanner(
+    'https://mongoosejs.com/',
+    'https://cdn.simpleicons.org/mongoose',
+    'Mongoose',
+  ),
 ]
 
-const jsBannersFactory = new Banner(jsNodeBanners)
+const jsBannersFactory = new Banner(jsBanners)
 
 document.write('<div class="u-flex u-gap-200 u-flex-wrap">')
-jsBannersFactory.renderRandomBanner(3)
+jsBannersFactory.renderRandomBanners(3)
 document.write('</div>')
