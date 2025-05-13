f3a3d3f4b60f975f0d7387872a78186d60b66f9f
BorisBedrenko <wadeforce@gmail.com>
2025-05-13 10:08:58 +0300

    feat(l24:t1): better regexp

--- src/lesson_24/tasks/scripts/task_01.js
+++ src/lesson_24/tasks/scripts/task_01.js
@@ -2,7 +2,7 @@
 
 class PhoneNumber {
   static regexp =
-    /^\+?(?:38)?[ -]?\(?(?<operatorCode>0\d{2})\)?[ -]?\d{3}[ -]?\d{2}[ -]?\d{2}$/
+    /^(?:\+?3?8[ -]?)?\(?(?<operatorCode>\d{3})\)?[ -]?\d{3}[ -]?\d{2}[ -]?\d{2}$/
 
   static operatorCodesByName = {
     Київстар: ['067', '068', '096', '097', '098', '077'],
@@ -86,7 +86,7 @@ class PhoneNumber {
       return `${this.number} – (${operatorName})`
     }
 
-    return this.number.replace(/[+() ]/g, '')
+    return Number(this.number.replace(/[+() ]/g, ''))
   }
 }
 
