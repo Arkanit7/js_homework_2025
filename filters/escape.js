96b45b91855f963c951ac35e3ccaed1a98c0f3fb
BorisBedrenko <wadeforce@gmail.com>
2025-06-03 13:33:34 +0300

    build: fix the escape filter

--- filters/escape.js
+++ filters/escape.js
@@ -2,7 +2,7 @@
  * @param {string} text
  */
 function escape(text) {
-  text.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+  return text.replace(/</g, '&lt;').replace(/>/g, '&gt;')
 }
 
 export default escape
